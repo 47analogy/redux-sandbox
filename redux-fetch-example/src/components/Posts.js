ffe5ad991a427549a005e976760b9a02dee92d48
analogy47 <actweed47@gmail.com>
2020-01-27 23:56:04 -0500

    add proptypes

--- redux-fetch-example/src/components/Posts.js
+++ redux-fetch-example/src/components/Posts.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions/postActions';
 
@@ -19,6 +20,12 @@ function Posts(props) {
     </div>
   );
 }
+
+Posts.prototypes = {
+  fetchPosts: PropTypes.func.isRequired,
+  posts: PropTypes.array.isRequired,
+};
+
 const mapStateToProps = state => ({
   posts: state.posts.items,
 });
